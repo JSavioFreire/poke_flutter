928342e81700064765542c8f1af70d027fb2e253
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2023-01-31 16:16:16 -0300

    finak

--- lib/components/widget/header/search.dart
+++ lib/components/widget/header/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+import 'package:poke_flutter/api/fetch_with_name.dart';
+import 'package:poke_flutter/components/widget/pokemons/loading.dart';
 import 'package:poke_flutter/theme/theme_colors.dart';
 
 class Search extends StatelessWidget {
@@ -8,14 +9,28 @@ class Search extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WithNameProvider controller = WithNameProvider();
+    TextEditingController textController = TextEditingController();
     return Container(
         decoration: BoxDecoration(
             color: ThemeColors.greyLight,
             borderRadius: BorderRadius.circular(10)),
         width: 200,
         child: TextFormField(
+          onEditingComplete: () {
+            controller.callApiPok(textController.text);
+            controller.data.value.isEmpty
+                ? const AlertDialog(
+                    title: Text('Dont Exist'),
+                  )
+                : controller.loadingData.value
+                    ? const LoadingPokemon()
+                    : Navigator.pushNamed(context, '/pokemon',
+                        arguments: controller.data.value);
+          },
+          controller: textController,
           decoration: const InputDecoration(
-              hintText: 'Search for one Pokemon',
+              hintText: 'Search name or number',
               border: InputBorder.none,
               contentPadding: EdgeInsets.only(left: 10)),
         ));
